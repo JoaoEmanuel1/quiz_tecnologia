fd46a8c1f79e27244bbd91846e7aee9df6b71bec
JoaoEmanuel1 <joaoemanuelcf01@gmail.com>
2025-05-15 15:59:37 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,69 @@
+#Importações e configuracoes básicas
+from flask import Flask, render_template, request, redirect, url_for, session
+from questions import questions
+import random
+
+app = Flask(__name__)
+app.secret_key = '427623'
+
+# Renderiza a tela inicial 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Inicia o quiz
+@app.route('/start')
+def start():
+
+    # embaralha as perguntas quando inicia
+    session['question_order'] = random.sample(range(len(questions)), len(questions))
+    session['current_question'] = 0
+    session['score'] = 0
+    return redirect(url_for('quiz'))
+
+@app.route('/quiz')
+def quiz():
+
+    # verificar se o quiz terminou
+    current_q = session['current_question']
+    if current_q >= len(questions):
+        return redirect(url_for('results'))
+    
+    # recebe a pergunta atual
+    question_data = questions[session['question_order'][current_q]]
+    
+    return render_template('quiz.html', 
+                         question=question_data['question'],
+                         options=question_data['options'],
+                         current=current_q + 1,
+                         total=len(questions))
+
+@app.route('/check', methods=['POST'])
+def check():
+
+    #recebe a resposta do usuário
+    selected_option = request.form.get('option')
+    current_q = session['current_question']
+    question_data = questions[session['question_order'][current_q]]
+    
+    # verifica se a resposta está correta
+    if selected_option == question_data['answer']:
+        session['score'] += 1
+    
+    # avança para a próxima pergunta
+    session['current_question'] += 1
+    
+    return redirect(url_for('quiz'))
+
+@app.route('/results')
+def results():
+    # Recupera o resultado final e mostra na tela
+    score = session['score']
+    total = len(questions)
+    
+    session.clear()
+    
+    return render_template('results.html', score=score, total=total)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ questions.py
@@ -0,0 +1,52 @@
+questions = [
+    {
+        'question': 'Qual palavra-chave é usada para herança em Java?',
+        'options': ['include', 'inherit', 'extends', 'implements', 'super'],
+        'answer': 'extends'
+    },
+    {
+        'question': 'Qual das opções abaixo é um tipo primitivo em Java?',
+        'options': ['String', 'Integer', 'char', 'Double', 'Boolean'],
+        'answer': 'char'
+    },
+    {
+        'question': 'Qual é a saída do código: System.out.println(3 + 4 + "Java"); ?',
+        'options': ['Java34', '7Java', '34Java', 'Java7', 'JavaJava'],
+        'answer': '7Java'
+    },
+    {
+        'question': 'Qual interface é usada para implementar uma fila (queue) em Java?',
+        'options': ['List', 'Deque', 'Queue', 'Set', 'Map'],
+        'answer': 'Queue'
+    },
+    {
+        'question': 'O que acontece se você tentar acessar um índice fora dos limites de um array?',
+        'options': ['O compilador ignora o erro', 'O Java corrige automaticamente', 'Lança uma exceção ArrayIndexOutOfBoundsException', 'Retorna null', 'O programa trava o sistema'],
+        'answer': 'Lança uma exceção ArrayIndexOutOfBoundsException'
+    },
+    {
+        'question': 'Qual é o modificador de acesso mais restritivo?',
+        'options': ['public', 'protected', 'default ', 'private', 'final'],
+        'answer': 'private'
+    },
+    {
+        'question': ' Qual é o resultado de 10 / 3 em Java com tipos inteiros?',
+        'options': ['3.333', '3.0', '3', '3.3333', 'Erro de compilação'],
+        'answer': '3'
+    },
+    {
+        'question': 'Qual das opções é uma interface funcional (funcionalidade do Java 8)?',
+        'options': ['ArrayList', 'Runnable', 'Scanner', 'BufferedReader', 'Thread'],
+        'answer': 'Runnable'
+    },
+    {
+        'question': 'Qual das opções permite criar uma thread em Java?',
+        'options': ['Estendendo a classe Object', 'Implementando a interface Threadable', 'Usando a classe Runnable', 'Chamando main() duas vezes', 'Criando um novo System'],
+        'answer': 'Usando a classe Runnable'
+    },
+    {
+        'question': 'Qual das opções abaixo não é um tipo de exceção verificada (checked exception)?',
+        'options': ['IOException', 'SQLException', 'FileNotFoundException', 'NullPointerException', 'ParseException'],
+        'answer': 'NullPointerException'
+    }
+]
